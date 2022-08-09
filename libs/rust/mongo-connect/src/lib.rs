b967698856d163d752e1ebe5e9c7be59f3dfbc44
yuri <krupnik.yuri@gmail.com>
2022-08-09 21:53:16 +0300

    no feature.

--- libs/rust/mongo-connect/src/lib.rs
+++ libs/rust/mongo-connect/src/lib.rs
@@ -1,18 +1,18 @@
 fn mongo_connect() -> String {
-    "mongo_connect".into()
+    "mongo_connect".to_string()
 }
 
 // fn options() {
 //   dbg!(hello("brooks"))
 // }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        assert_eq!(mongo_connect(), "mongo_connect".to_string());
-        // let unwrapped_name =
-    }
-}
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//
+//     #[test]
+//     fn it_works() {
+//         assert_eq!(mongo_connect(), "mongo_connect".to_string());
+//         // let unwrapped_name =
+//     }
+// }
