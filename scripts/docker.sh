66ab84a7ccc84695a5f34997e6be064b0db0d4f1
yuri <krupnik.yuri@gmail.com>
2022-08-10 01:56:39 +0300

    no feature.

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -4,8 +4,6 @@ set -e
 ls
 docker buildx build \
   --target scratch \
-  --build-arg DIST_PATH=target/release/users \
   --push \
   --platform linux/amd64,linux/arm64 . \
-  -t yurikrupnik/rust-users-api:"$TAG" \
   -t europe-west1-docker.pkg.dev/maya-mussia/nx-go-playground/rust-users-api:"$TAG" \
