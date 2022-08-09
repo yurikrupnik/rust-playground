d93ff95e236e23ec688ff9b9768a4b8e9af3a26a
yuri <krupnik.yuri@gmail.com>
2022-08-09 22:44:58 +0300

    no feature.

--- apps/rust/users/src/main.rs
+++ apps/rust/users/src/main.rs
@@ -1,4 +1,7 @@
-// mod libs::rust
+// mod mongo-connect;
+// use crate::libs::rust::mongo-connect;
+
+const ECHO_SERVER_ADDRESS: &str = "localhost:1234";
 
 fn main() {
     println!("Hello, world!");
