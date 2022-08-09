7bc784734041df0ce5e158781c3b2740a78f4442
yuri <krupnik.yuri@gmail.com>
2022-08-10 01:38:23 +0300

    no feature.

--- apps/personal/blog/specs/index.spec.tsx
+++ apps/personal/blog/specs/index.spec.tsx
@@ -5,7 +5,6 @@ import Index from '../pages/index';
 
 describe('Index', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Index />);
-    expect(baseElement).toBeTruthy();
+    render(<Index />);
   });
 });
