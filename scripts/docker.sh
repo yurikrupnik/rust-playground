cb1d44f2549214d597fc5f951cfdbd6f9c7dfcc7
yuri <krupnik.yuri@gmail.com>
2022-08-15 02:39:05 +0300

    no feature.

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -6,7 +6,6 @@ echo v1 $1
 echo v12 $2
 echo v11 $3
 docker buildx build \
-#  --target scratch \
-#  --platform linux/amd64,linux/arm64 . \
-#  -t europe-west1-docker.pkg.dev/maya-mussia/nx-go-playground/rust-users-api \
-#  --push \
+  --platform linux/amd64,linux/arm64 . \
+  -t europe-west1-docker.pkg.dev/maya-mussia/nx-go-playground/rust-users-api \
+  --push
